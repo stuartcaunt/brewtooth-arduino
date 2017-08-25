7041d8dcd396e63d118870afb532ef2722997070
Stuart Caunt <caunt@cauntbook.gen.ill.fr>
2017-08-25 09:43:14 +0200

    Start of wifi access point for configuration.

--- src/app/WifiConfigurationStation.cpp
+++ src/app/WifiConfigurationStation.cpp
@@ -1,12 +1,43 @@
 #include "WifiConfigurationStation.h"
 #include "../utils/Configuration.h"
+#include <ESP8266Wifi.h>
+#include <ESP8266WebServer.h>
+
+WifiConfigurationStation::WifiConfigurationStation() :
+    _server(new ESP8266WebServer(80)) {
+
+}
+
+WifiConfigurationStation::~WifiConfigurationStation() {
+    delete _server;
+}
 
 void WifiConfigurationStation::setup() {
-    Configuration::properties.wifiData.isConfigured = true;
-    Configuration::save();
+
+    String macAddress = WiFi.softAPmacAddress();
+    Serial.println("Mac addr = " + macAddress);
+    String ssid = "BREWTOOTH-" + macAddress.substring(0, 4);
+
+    /* You can remove the password parameter if you want the AP to be open. */
+    WiFi.softAP(ssid.c_str());
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(myIP);
+
+    _server->on("/", std::bind(&WifiConfigurationStation::handleRoot, this));
+    
+    _server->begin();
+    Serial.println("HTTP server started");
+
+    // Configuration::properties.wifiData.isConfigured = true;
+    // Configuration::save();
 
 }
 
 void WifiConfigurationStation::loop() {
     
+}
+
+void WifiConfigurationStation::handleRoot() {
+    _server->send(200, "text/html", "<h1>You are connected</h1>");
 }
--- src/app/WifiConfigurationStation.h
+++ src/app/WifiConfigurationStation.h
@@ -3,15 +3,22 @@
 
 #include "ApplicationState.h"
 
+class ESP8266WebServer;
+
 class WifiConfigurationStation : public ApplicationState {
 public:
-    WifiConfigurationStation() {}
-    virtual ~WifiConfigurationStation() {}
+    WifiConfigurationStation();
+    virtual ~WifiConfigurationStation();
 
     virtual void setup();
     virtual void loop();
 
 private:
+    void handleRoot();
+        
+private:
+    ESP8266WebServer * _server;
+    
 };
 
 #endif /*WIFICONFIGURATIONSTATION_H*/
