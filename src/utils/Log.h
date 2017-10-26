c806f6633ba0fac9bfe70c3aaca7b9fa6aabe2b9
Stuart Caunt <stuart.caunt@gmail.com>
2017-10-26 20:21:36 +0200

    Change logging to error only

--- src/utils/Log.h
+++ src/utils/Log.h
@@ -7,7 +7,7 @@
 #define LOG_LEVEL_ERROR 3
 #define LOG_LEVEL_NONE 99
 
-#define LOG_LEVEL LOG_LEVEL_INFO
+#define LOG_LEVEL LOG_LEVEL_ERROR
 
 #if (LOG_LEVEL == LOG_LEVEL_DEBUG)
 #define DEBUG(__fmt, ...) Serial.printf("[DEBUG] %4d %-50s " __fmt"\n", __LINE__, __FILE__, ##__VA_ARGS__)
